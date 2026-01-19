2f58eafddc77485e46f3b8c6ffc3d571e6020998
SPkHz <232274993+SPkHz@users.noreply.github.com>
2026-01-19 08:25:33 +0000

    Deploying to gh-pages from @ SPkHz/SPkHz.github.io@288a503a02ff2ebcc97f85deac469f54f0c4771f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -450,32 +450,7 @@ ninja.data = [{
           section: "News",},{id: "news-digital-signal-processing-design-project-see-the-ee-302-fir-hearing-aid-filter-design-project-page-for-more-details",
           title: 'Digital Signal Processing Design Project See the EE-302 FIR Hearing Aid Filter Design...',
           description: "",
-          section: "News",},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-optimized-silicon-solar-cell-design",
+          section: "News",},{id: "projects-optimized-silicon-solar-cell-design",
           title: 'Optimized Silicon Solar Cell Design',
           description: "Single-junction silicon PV cell optimized in ANSYS Lumerical DEVICE (AM1.5). 16.08% efficiency with Si3N4 ARC + Al contacts, plus cost and sustainability analysis.",
           section: "Projects",handler: () => {

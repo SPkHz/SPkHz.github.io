7d3c618fc253217af8b29e2fdf185cda2118e9ec
SPkHz <232274993+SPkHz@users.noreply.github.com>
2026-01-19 20:17:06 +0000

    Deploying to gh-pages from @ SPkHz/SPkHz.github.io@4e81eedefc9f165cdd7037a4fc7b0027fafb184f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -668,8 +668,8 @@ ninja.data = [{
           description: "First-order RC high-pass filter characterization (theory vs. LTspice vs. WaveForms measurements) with MATLAB Bode magnitude/phase plots.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/ee319_lab_01_high_pass_filter/";
-            },},{id: "projects-ee-lab-ii-final-lab-portfolio-spring-2025",
-          title: 'EE Lab II Final Lab Portfolio Spring 2025',
+            },},{id: "projects-ee-lab-ii-final-portfolio-spring-2025",
+          title: 'EE Lab II Final Portfolio Spring 2025',
           description: "A complete compendium of labs from the spring &#39;25 semester, lab 01 through lab 08 (Please use ToC to navigate, everything is hyperlinked!) --&gt; MOSFET characterization → amplifier design → differential pairs → VNA S-parameter de-embedding (LTspice • Python • LaTeX).",
           section: "Projects",handler: () => {
               window.location.href = "/projects/ee322_final_lab_portfolio/";

5cb86d88599c84b092d0aee34f5973fc34a80ba2
SPkHz <232274993+SPkHz@users.noreply.github.com>
2026-01-20 07:37:37 +0000

    Deploying to gh-pages from @ SPkHz/SPkHz.github.io@e5ba360252ab946ecd9e3b269240cbd289485bad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-design-project-portfolio",
+          title: "Design Project Portfolio",
           description: "A growing collection of all of my electrical engineering design projects and coursework.",
           section: "Navigation",
           handler: () => {
